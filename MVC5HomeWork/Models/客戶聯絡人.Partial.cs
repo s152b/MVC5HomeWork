f825d89d973d64e4fe450b248a595876e719ecf5
chi <siro228@gmail.com>
2017-03-12 21:52:00 +0800

    對於 Create 與 Edit 表單要套用欄位驗證 (必填、Email格式、欄位長度限制等驗證)

--- MVC5HomeWork/Models/客戶聯絡人.Partial.cs
+++ MVC5HomeWork/Models/客戶聯絡人.Partial.cs
@@ -26,6 +26,7 @@ namespace MVC5HomeWork.Models
         
         [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
